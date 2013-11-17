ebe5dcd4ab16ceb521281d76ee5fb7ea930d34d4
Alfie Hanssen <alfiehanssen@gmail.com>
2013-11-17 14:47:34 -0500

    added objcbasics

--- ObjcBasics/ObjcBasics/main.m
+++ ObjcBasics/ObjcBasics/main.m
@@ -12,11 +12,63 @@
 
 int main(int argc, char * argv[])
 {
-    NSLog(@"Hello, World!");
-    
-//    @autoreleasepool {
-//        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-//    }
-    
-    return 0;
+    @autoreleasepool {
+ 
+        
+        // Example #1
+        
+        NSString *name = @"Alfie";
+        int age = 34;
+        float height = 6 + 4.0f/12;
+        
+        NSLog(@"My name is %@. I am %i years old. I am %.2f feet tall.", name, age, height);
+        
+        
+        // Example #2
+        
+        BOOL hasPrettyLongName = NO;
+        
+        if ([name length] > 10) {
+            hasPrettyLongName = YES;
+        } else {
+            hasPrettyLongName = NO;
+        }
+        
+        NSLog(@"Is my name pretty long? %i (%i characters)", hasPrettyLongName, [name length]);
+
+        
+        // Example #3
+        
+        NSArray *numbers = @[@10, @20, @25, @100];
+        
+        float sum = 0.0f;
+        
+        for (int i = 0; i < [numbers count]; i++) {
+            NSNumber *number = numbers[i];
+            sum = sum + [number intValue];
+        }
+        
+        float average = sum / [numbers count]; // Should be 38.75
+        
+        NSLog(@"average = %.2f", average);
+        
+        
+        // Example #4
+        
+        NSDictionary *myInfo = @{@"name":@"Alfie",
+                               @"age":@34,
+                               @"height":@6.33};
+
+        NSArray *allKeys = [myInfo allKeys];
+        
+        for (int i = 0; i < [allKeys count]; i++) {
+            
+            NSString *key = [allKeys objectAtIndex:i];
+            id value = [myInfo valueForKey:key];
+            
+            NSLog(@"%@ : %@", key, value);
+        }
+        
+//        UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
 }
